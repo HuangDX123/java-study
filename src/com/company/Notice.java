835a2c279126c41286a5badb20696d8ce66370a0
初尧 <45021179+HuangDX123@users.noreply.github.com>
2020-08-17 23:20:02 +0800

    添加提示

---
+++ src/com/company/Notice.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Notice {
+	public static void main(String[] args) {
+		// 比如说你新建了一个文件，比如说这个文件，跟着教程走完
+		// 点上面的VCS - -提交，或者⌘K，在这填写大概修改了什么内容，然后点击commit
+//		然后VCS-Git-Push（Shift command K），push就行了
+//		我演示一遍
+	}
+}
