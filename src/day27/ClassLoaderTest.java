c06da772e86d28fb1a1eb743179ba76266f665a4
初尧 <45021179+HuangDX123@users.noreply.github.com>
2020-09-07 09:36:16 +0800

    更新目录

--- src/day27/ClassLoaderTest.java
+++ src/day27/ClassLoaderTest.java
@@ -1,5 +1,6 @@
 package day27;
 
+
 import org.junit.Test;
 
 import java.io.InputStream;
--- src/day27/FieldTest.java
+++ src/day27/FieldTest.java
@@ -14,7 +14,7 @@ public class FieldTest {
     @Test
     public void test1(){
 
-        Class clazz = Person.class;
+        Class clazz = day27.Person.class;
 
         //获取属性结构
         //getFields():获取当前运行时类及其父类中声明为public访问权限的属性
@@ -34,7 +34,7 @@ public class FieldTest {
     //权限修饰符  数据类型 变量名
     @Test
     public void test2(){
-        Class clazz = Person.class;
+        Class clazz = day27.Person.class;
         Field[] declaredFields = clazz.getDeclaredFields();
         for(Field f : declaredFields){
             //1.权限修饰符
